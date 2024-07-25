1c42ae7ce921d29ab9359ba4be98e234da3e8799
shaiz2021 <mshahzaibkk2003@gmail.com>
2024-07-25 14:54:36 +0500

    update

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -22,11 +22,11 @@ const Portfolio = () => {
                 {
                     projects.map(project => 
                     <div key={project.id} className="shadow-xl rounded-2xl cursor-pointer bg-bgShade">
-                        <img src={project.image} alt="" className="hover:scale-90 transition-all duration-300 rounded-2xl" />
+                        <img src={project.image} alt="portfolioimages" className="hover:scale-90 transition-all duration-300 rounded-2xl" />
                         <div className="p-8">
                             <h3 className="text-2xl font-semibold mb-2 text-headingColor">{project.name}</h3>
                             <p className="text-body mb-4">{project.description}</p>
-                            <a href={project.link} className="font-bold underline underline-offset-8 hover:text-primary "> View <img src="/src/assets/Vector.png" alt="" className="w-3 inline-block ml-3" /> </a>
+                            <a href={project.link} className="font-bold underline underline-offset-8 hover:text-primary "> View <img src="/src/assets/Vector.png" alt="vector" className="w-3 inline-block ml-3" /> </a>
                         </div>
                     </div>)
                 }
