f3d670610fb740cebf2ed39ba4ed73c652b3efa5
shaiz2021 <mshahzaibkk2003@gmail.com>
2024-08-03 14:48:43 +0500

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/portfolio/",
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
 })
