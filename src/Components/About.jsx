2e17c92e5d75d014b4b8b5d4c09f9d56144190b6
shaiz2021 <mshahzaibkk2003@gmail.com>
2024-08-10 10:05:48 +0500

    asas

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,11 +1,12 @@
-import PortfolioImage from '../assets/ForCV.png'
-
 const About = () => {
+    const about = [
+        {image: "/src/assets/ForCV.png"}
+        ]
     return (
         <div className="lg-mx-12 mx-4" id='about'>
             <div className='flex flex-col sm:flex-row md:gap-12 gap-12 items-center justify-between py-20'>
                 <div className='sm:w-1/2'>
-                    <img src={PortfolioImage} alt="" className='w-90 sm:w-11/12 rounded-full' />
+                <img src={about.image} alt="aboutimage" className='w-90 sm:w-11/12 rounded-full' />
                 </div>
                 <div className="text-headingColor mb-20 sm:w-1/2 bg-bgShade rounded-lg shadow-xl p-7">
                     <h2 className="md:text-5xl text-4xl font-bold">About Me</h2><br />
